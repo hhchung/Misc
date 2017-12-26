940bf6a732e6cb5111665ca0bd3e77a858264b9d
hhchung <hhchung.cs01@g2.nctu.edu.tw>
2017-12-26 12:40:21 +0800

    add gcc builtin lock example

---
+++ gcc_builtin_lock/lock.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <pthread.h>
+
+
+#ifdef SYS_gettid
+#define gettid() syscall(SYS_gettid)
+#else
+#error "SYS_gettid unavailable on this system"
+#endif
+
+
+
+//a simple userspace lock
+
+int mlock;
+int var;
+void lock(void){
+    while(__sync_lock_test_and_set(&mlock, 1)) while(mlock);
+}
+void unlock(void){
+    __sync_lock_release(&mlock);
+}
+
+void* worker(void *arg){
+    int i;
+    
+    for(i = 0 ; i < 100000;i++){
+        lock();
+        var+=1;
+        printf("%ld add var=%d\n",gettid(), var);
+        unlock();
+    }    
+    pthread_exit(NULL);
+}
+
+
+
+int main(int argc, char *argv[]){
+    pthread_t th1, th2;
+    var = 0;
+    
+    pthread_create(&th1, NULL, worker, NULL);
+    pthread_create(&th2, NULL, worker, NULL);
+    
+    pthread_join(th1, NULL);
+    pthread_join(th2, NULL);
+    printf("Final %d\n",var);
+    if(var != 200000){
+        printf("Lock fail QQ\n");
+    }else{
+        printf("Lock success\n");
+
+    }
+
+    return 0;
+}
+
